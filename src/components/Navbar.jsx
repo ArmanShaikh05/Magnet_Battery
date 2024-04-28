015e2db2d3a327bf0a4a7203467a5c1ed65d2661
ArmanShaikh05 <arman.shaikh01082003@gmail.com>
2024-04-28 21:01:34 +0530

    minor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,7 +64,7 @@ const Navbar = () => {
           <svg onClick={(e)=>handleHideSidebarEvent(e)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
           <div className="sidebar-links">
           <Link onClick={()=>setShowSidebar(false)} to="/">Home</Link>
-            <a onClick={()=>setShowSidebar(false)} href="#about">About us</a>
+            <a onClick={()=>setShowSidebar(false)} href="/#about">About us</a>
             <Link onClick={()=>setShowSidebar(false)} to={"/contact"}>Contact Us</Link>
             <button className="btn" onClick={(e)=>handleNavigate(e)} >Our Store</button>
           </div>
