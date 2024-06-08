52616603e7a6a738b9447c77e926299512e32b7e
ArmanShaikh05 <arman.shaikh01082003@gmail.com>
2024-06-08 23:56:07 +0530

    minor bugs

--- src/components/store/InverterItem.jsx
+++ src/components/store/InverterItem.jsx
@@ -1,9 +1,11 @@
 import { useNavigate } from "react-router-dom"
+import { useSidebarContextHook } from "../../context/contextHooks"
 
 /* eslint-disable react/prop-types */
 const InverterItem = ({itemData}) => {
   const navigate = useNavigate()
   const discount = (((itemData.boxPrice - itemData.sellingPrice) / itemData.boxPrice)*100).toFixed(1)
+  const {setShowSidebar} = useSidebarContextHook()
   return (
     <div className="store-item" onClick={()=>{window.scrollTo(0,0),navigate(`/item/inverter/${itemData._id}`)}}>
         <div className="store-item-img"><img src={itemData.image.url} alt="amaronBattery" /></div>
@@ -16,7 +18,7 @@ const InverterItem = ({itemData}) => {
             </div>
             <div className="compatible-vehicles">
                 <div></div>
-                <button onClick={()=>{window.scrollTo(0,0),navigate(`/item/inverter/${itemData._id}`)}}  className="btn">View details</button>
+                <button onClick={()=>{window.scrollTo(0,0),setShowSidebar(false),navigate(`/item/inverter/${itemData._id}`)}}  className="btn">View details</button>
             </div>
         </div>
     </div>
--- src/components/store/StoreItem.jsx
+++ src/components/store/StoreItem.jsx
@@ -1,9 +1,11 @@
 import { useNavigate } from "react-router-dom"
+import { useSidebarContextHook } from "../../context/contextHooks"
 
 /* eslint-disable react/prop-types */
 const StoreItem = ({itemData}) => {
   const navigate = useNavigate()
   const discount = (((itemData.boxPrice - itemData.sellingPrice) / itemData.boxPrice)*100).toFixed(1)
+  const {setShowSidebar} = useSidebarContextHook()
   return (
     <div className="store-item" onClick={()=>{window.scrollTo(0,0),navigate(`/item/${itemData._id}`)}}>
         <div className="store-item-img"><img src={itemData.image.url} alt="amaronBattery" /></div>
@@ -16,7 +18,7 @@ const StoreItem = ({itemData}) => {
             </div>
             <div className="compatible-vehicles">
                 <p><strong>Compatible with - </strong>{itemData.vehicles}</p>
-                <button onClick={()=>{window.scrollTo(0,0),navigate(`/item/${itemData._id}`)}}  className="btn">View details</button>
+                <button onClick={()=>{window.scrollTo(0,0),setShowSidebar(false),navigate(`/item/${itemData._id}`)}}  className="btn">View details</button>
             </div>
         </div>
     </div>
